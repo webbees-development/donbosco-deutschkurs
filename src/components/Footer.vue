15b4bd1080b49886fd9d93e90fb42ed736b02740
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2022-11-21 13:04:42 +0100

    add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,214 @@
+<template>
+  <footer>
+    <div class="footer_content_1">
+      <a class="logo" href="#"
+        ><g-image src="../assets/images/logo.png" alt="Don Bosco Logo" />
+      </a>
+    </div>
+    <div class="footer_content_2">
+      <div>
+        <h3>
+          Wir helfen Flüchtlingen in Wien, um <br />Perspektiven zu eröffnen.
+          Sei’ auch dabei.
+        </h3>
+        <button>Probetermin vereinbaren</button>
+      </div>
+
+      <nav>
+        <ul aria-label="Primary" role="list" class="nav-list">
+          <li><a href="#hero">Startseite</a></li>
+          <li><a href="#about-us">Über uns</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#photos">Fotos</a></li>
+        </ul>
+      </nav>
+
+      <div id="contact" class="contact">
+        <div>
+          <h4>Don Bosco für Flüchtlinge</h4>
+          <p>Hagenmüllergasse 31</p>
+          <p>1030 Wien</p>
+          <p>
+            Mail:
+            <a href="mailto:donbosco.deutschkurs@gmail.com"
+              >donbosco.deutschkurs@gmail.com</a
+            >
+          </p>
+        </div>
+
+        <div class="social-media">
+          <a
+            class="facebook"
+            href="https://www.facebook.com/donbosco.fuerfluechtlinge"
+          >
+          </a
+          ><a
+            class="instagram"
+            href="https://www.instagram.com/donbosco4refugees/"
+          >
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="footer_content_3">
+      <p>© <span id="year"></span> Don Boscos für Flüchtlinge</p>
+      <p><a>Impressum</a> | <a>Datenschutz</a></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  mounted() {
+    document.getElementById(
+      "year"
+    ).innerHTML = new Date().getFullYear().toString();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/global.scss";
+
+footer {
+  color: var(--clr-primary);
+  font-family: var(--ff-primary);
+  background-image: url("../assets/images/bg_img.png");
+  padding-block: 4.5rem 1rem;
+}
+
+h3 {
+  margin-bottom: 1.5rem;
+}
+
+h4 {
+  font-size: var(--fs-200);
+  font-style: italic;
+  font-weight: bold;
+  line-height: 1.4;
+}
+
+nav ul {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+p {
+  font-size: var(--fs-200);
+  font-style: normal;
+  line-height: 1.5;
+  color: inherit;
+
+  strong {
+    opacity: 0.6;
+    font-weight: 600;
+  }
+}
+
+nav {
+  a {
+    transition: filter 200ms ease;
+  }
+
+  a:hover,
+  a:focus-visible {
+    filter: brightness(140%);
+  }
+
+  @media (max-width: $tablet-break) {
+    font-size: var(--fs-200);
+    line-height: 1.6;
+  }
+}
+
+.footer_content_1,
+.footer_content_2,
+.footer_content_3 {
+  padding-inline: var(--padding-global-inline);
+}
+
+.footer_content_2 {
+  display: flex;
+  justify-content: space-between;
+  padding-block: 2.5rem 7rem;
+  white-space: nowrap;
+
+  @media (max-width: 960px) {
+    nav {
+      display: none;
+    }
+  }
+
+  @media (max-width: 850px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+
+    padding-block-end: 0.4rem;
+
+    h3 {
+      margin-bottom: 0;
+    }
+    button {
+      display: none;
+    }
+  }
+}
+.footer_content_3 {
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid var(--clr-primary);
+  padding-block-start: 1rem;
+
+  p {
+    font-size: var(--fs-200);
+  }
+
+  @media (max-width: 500px) {
+    flex-direction: column-reverse;
+    text-align: center;
+
+    p:first-child {
+      padding-block-start: 0.4rem;
+    }
+  }
+}
+.logo {
+  display: block;
+  max-width: 9.5rem;
+
+  @media (max-width: 850px) {
+    margin-inline: auto;
+  }
+}
+.contact {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media (max-width: 850px) {
+    padding-block: 4rem;
+  }
+}
+.social-media {
+  padding-block-start: 0.6rem;
+}
+.facebook,
+.instagram {
+  background-repeat: no-repeat;
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+}
+.facebook {
+  background-image: url("../assets/images/facebook-f.svg");
+  padding-right: 2.5rem;
+}
+.instagram {
+  background-image: url("../assets/images/instagram.svg");
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="layout">
     <slot />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from "../components/Footer.vue";
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
 
 <style></style>
