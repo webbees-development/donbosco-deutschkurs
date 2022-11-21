66dcae70376d48b786193d7c4d39b2a2cec360de
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2022-11-21 14:08:14 +0100

    add Appointment

---
+++ src/components/Appointment.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="appointment">
+    <div class="content">
+      <h2>
+        Tu anderen und<br />
+        damit auch dir selbst etwas Gutes.
+      </h2>
+      <p>
+        Nimm dir 2 Stunden, teile deine Sprache Deutsch und bereichere damit
+        (d)ein ganzes Leben.
+      </p>
+      <button>Probetermin vereinbaren</button>
+    </div>
+    <div class="image">
+      <g-image src="../assets/images/haende.jpg" />
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/global.scss";
+.appointment {
+  background-image: url("../assets/images/bg_img.png");
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding-block: 3.75rem;
+}
+.content {
+  padding-inline-end: min(10.5vw, 12.5rem);
+
+  @media (max-width: $first-break) {
+    padding-inline-end: 8vw;
+  }
+}
+h2 {
+  max-width: 16ch;
+}
+p,
+button {
+  margin-inline-start: 2.5rem;
+}
+
+p {
+  margin-block: 3.25rem 2.5rem;
+  max-width: 28ch;
+}
+
+.image {
+  width: min(50vw, 61rem);
+  aspect-ratio: 1.3;
+  object-fit: cover;
+
+  @media (max-width: 1600px) {
+    aspect-ratio: 1.2;
+  }
+  @media (max-width: 1400px) {
+    aspect-ratio: 1.3;
+  }
+}
+
+@media (max-width: $tablet-break) {
+  .appointment {
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    padding-block: 7rem;
+    padding-inline: var(--padding-global-inline);
+  }
+  .content {
+    padding: 0;
+  }
+  h2 {
+    max-width: 20ch;
+    margin-inline: auto;
+    br {
+      display: none;
+    }
+  }
+  p,
+  button {
+    margin-inline-start: 0;
+  }
+  p {
+    margin-block: 2.8rem 2rem;
+    max-width: 40ch;
+  }
+  .image {
+    display: none;
+  }
+}
+</style>
