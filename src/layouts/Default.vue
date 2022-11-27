d998e01d7d8181f3b63151070fc7975c9c968b60
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2022-11-27 22:25:03 +0100

    add component Header to Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="layout">
+    <Header />
     <main>
       <slot />
     </main>
@@ -8,9 +9,11 @@
 </template>
 
 <script>
+import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 export default {
   components: {
+    Header,
     Footer,
   },
 };
