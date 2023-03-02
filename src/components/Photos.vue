db103797e58bc48a624d4d651010bfadfac997c0
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-02 15:34:03 +0100

    add component Photos

---
+++ src/components/Photos.vue
@@ -0,0 +1,158 @@
+<template>
+  <section id="photos" class="photos">
+    <h2>Freude erleben und teilen:</h2>
+    <p>
+      Unsere Fotogalerie ist bunt und voller Leben.<br />
+      Schau’ dich gerne um:
+    </p>
+    <div class="photos-grid">
+      <div class="photo">
+        <g-image src="../assets/images/photos-1.jpg" alt=""></g-image>
+      </div>
+      <div class="photo">
+        <g-image src="../assets/images/photos-2.jpg" alt=""></g-image>
+      </div>
+      <div class="photo">
+        <g-image src="../assets/images/photos-4.jpg" alt=""></g-image>
+      </div>
+      <div class="photo">
+        <g-image src="../assets/images/photos-9.jpg" alt=""></g-image>
+      </div>
+      <div class="photo">
+        <g-image src="../assets/images/photos-3.jpg" alt=""></g-image>
+      </div>
+      <div class="photo">
+        <g-image src="../assets/images/photos-8.jpg" alt=""></g-image>
+      </div>
+      <div class="photo">
+        <g-image src="../assets/images/photos-5.jpg" alt=""></g-image>
+      </div>
+    </div>
+    <g-link class="btn" to="/photos">Zu allen Fotos</g-link>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/styles/global.scss";
+
+.photos {
+  text-align: center;
+  padding: 9rem min(16vw, 17.5rem);
+
+  br {
+    display: none;
+  }
+  @media (max-width: $tablet-break) {
+    padding-inline: var(--padding-global-inline);
+  }
+
+  @media (max-width: $mobile-break) {
+    br {
+      display: block;
+    }
+  }
+}
+.photos h2 {
+  padding-bottom: 0.5rem;
+}
+
+.photos-grid {
+  padding-block: 3rem 4rem;
+  display: grid;
+  gap: 1.5rem;
+
+  grid-template-columns: repeat(12, 1fr);
+
+  @media (max-width: $tablet-break) {
+    gap: 1rem;
+  }
+  @media (max-width: $mobile-break) {
+    grid-template-columns: repeat(2, 1fr);
+    .photo:nth-of-type(even) {
+      grid-column: 2/5;
+      img {
+        aspect-ratio: 1;
+      }
+    }
+    .photo:nth-of-type(odd) {
+      grid-column: 1/2;
+      img {
+        aspect-ratio: 1;
+      }
+    }
+    .photo:last-child {
+      display: none;
+    }
+  }
+  @media (max-width: 400px) {
+    display: block;
+    padding-block: 2.5rem 2rem;
+
+    .photo:nth-child(n) {
+      img {
+        aspect-ratio: 1.4;
+        padding-block-end: 2rem;
+      }
+    }
+  }
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.photo:nth-child(1) {
+  grid-column: 1/8;
+
+  img {
+    aspect-ratio: 9/2;
+  }
+}
+.photo:nth-child(2) {
+  grid-column: 8/13;
+
+  img {
+    aspect-ratio: 1.4;
+  }
+}
+.photo:nth-child(3) {
+  grid-column: 1/5;
+
+  img {
+    aspect-ratio: 1;
+  }
+}
+
+.photo:nth-child(4) {
+  grid-column: 5/9;
+
+  img {
+    aspect-ratio: 1;
+  }
+}
+
+.photo:nth-child(5) {
+  grid-column: 9/13;
+
+  img {
+    aspect-ratio: 1;
+  }
+}
+
+.photo:nth-child(6) {
+  grid-column: 1/6;
+
+  img {
+    aspect-ratio: 1.4;
+  }
+}
+.photo:nth-child(7) {
+  grid-column: 6/13;
+
+  img {
+    aspect-ratio: 9/2;
+  }
+}
+</style>
