2fb1879339936f17c6329e7ce7a3bf5e6464288a
RiDoVu <106874014+RiDoVu@users.noreply.github.com>
2023-03-07 11:07:54 +0100

    add calendly link

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,12 +5,15 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+module.exports = function(api) {
+  api.loadSource(async (store) => {
+    store.addMetadata("url", "https://calendly.com/donbosco-deutschkurs/30min");
+  });
+  // api.loadSource(({ addCollection }) => {
+  //   // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  // });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+  });
+};
